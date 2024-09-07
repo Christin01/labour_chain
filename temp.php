799f4e80d190118464cbceb9fd7979fdacc2b566
Christin01 <97187118+Christin01@users.noreply.github.com>
2024-09-07 14:59:31 +0530

    Add files via upload

---
+++ temp.php
@@ -0,0 +1,247 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<title>Migrant Labourers  | Register :: Trinity Technologies</title>
+<!-- for-mobile-apps -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Migrant Labourers Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+		function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- //for-mobile-apps -->
+<link href="temp/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="temp/css/font-awesome.css" rel="stylesheet"> 
+<link rel="stylesheet" href="temp/css/jquery-ui.css" />
+<link href="temp/css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Raleway:400,500,600,700,800,900" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Dancing+Script:400,700" rel="stylesheet">
+<!--//fonts-->
+<script type="text/javascript">
+    function nme(b2)
+{
+var k5=b2.length;
+var ch2=/([a-z])$/;
+if(ch2.test(b2)==false)
+{
+document.getElementById("np3").innerHTML="<font color='red'>*Only Alphabets*</font>";
+$("#btn").hide();
+return false;
+}
+
+else
+{
+  document.getElementById("np3").innerHTML="";  
+  $("#btn").show();
+}
+}
+  
+    
+    function chkc(b2)
+{
+var k5=b2.length;
+var ch2=/([0-9])$/;
+if(ch2.test(b2)==false)
+{
+document.getElementById("o3").innerHTML="<font color='red'>*Only Numbers*</font>";
+$("#btn").hide();
+return false;
+}
+else if(k5!=10)
+{
+document.getElementById("o3").innerHTML="<font color='red'>*Please Check Your Mobile Number*</font>";
+$("#btn").hide();
+return false;
+}
+else
+{
+  document.getElementById("o3").innerHTML="";  
+  $("#btn").show();
+}
+}
+  
+ function chkp(c)
+{
+var s=document.getElementById("p10").value;
+
+if(s==c)
+{
+document.getElementById("p").innerHTML="<font color='Green'>*Password is Correct*</font>";
+$("#btn").show();
+return false;
+}
+else
+{
+document.getElementById("p").innerHTML="<font color='red'>*Verfy Password*</font>";
+$("#btn").hide();
+}
+}
+
+
+
+
+
+function vem(a)  
+     {  
+          //var a = document.myform.email.value;  
+          var atposition = a.indexOf("@");  
+          var dotposition = a.lastIndexOf(".");  
+          if (atposition<1 || dotposition<atposition+2 || dotposition+2>=a.length) 
+          {  
+               document.getElementById("em").innerHTML="<font color='red'>*Please Check Your Email Address*</font>";
+                $("#btn").hide();  
+          }  
+          else
+{
+                document.getElementById("em").innerHTML="";  
+  $("#btn").show();
+}
+     }  
+    </script>
+</head>
+<body>
+<!--banner start here-->
+<div class="banner-w3ls inner-banner-agileits" id="home">
+	<div class="container">
+		<!-- header -->
+                 <?php
+                 
+                 include 'menu.php';
+                 ?>
+</div>			
+</div>
+<!--//home-->
+<!--banner end here-->
+<!-- breadcrumbs -->
+
+<!-- //breadcrumbs -->
+<div class="login elite-app">
+	<div class="container">
+	<div class="tittle-agileinfo">
+				<h3>USer Register</h3>
+                </div>
+	<div class="col-md-8 login-form-w3-agile">
+			<form  method="post" enctype="multipart/form-data">
+				
+                               
+				<div class="w3_form_body_grid w3_form_body_grid1">
+					<span>Name</span>
+					<input type="text" name="t1" placeholder="Name" required="">
+				</div>
+				
+				
+                               
+                               <div class="w3_form_body_grid">
+					<span>Contact</span>
+                                        <input class type="text" name="t2" placeholder="Contact Info" required="" onkeyup="chkc(this.value)"><b id="o3"></b>
+				</div>
+				<div class="w3_form_body_grid">
+					<span>Email</span>
+                                        <input type="email" name="t3" placeholder="Email" required=""onkeyup="vem(this.value)"><b id="em"></b>
+				</div>
+				<div class="w3_form_body_grid w3_form_body_grid1">
+					<span>Password</span>
+					<input type="password" name="t4" placeholder="Password" required="">
+				</div>
+                               <div class="w3_form_body_grid">
+					<span>Account no</span>
+					<input class="form-control"style="background-color: black" type="number" name="t5" placeholder="Your Account number" required="">
+				</div>
+                            
+                               <div class="w3_form_body_grid w3_form_body_grid1">
+					<span>Photo</span>
+					<input name="myFile" class="frm-field required" class="form-control" type="file"required="">
+				</div>
+                              
+				<input name="sub1" type="submit" value="Sign Up">
+			</form>
+			
+		</div>
+            <div class="col-md-4">
+                
+                <img style="width: 100%;height: 540px" src="pic/1v2.gif">
+                
+            </div>
+            
+	</div>
+</div>
+<!--login-inner-->
+<!-- footer -->
+
+<div class="footer-w3layouts">
+				<div class="container">
+				<div class="agile-copy">
+					<p>© 2021 Migrant Labourers. All rights reserved | Design by <a href="#">Trinity Technologies</p>
+				</div>
+				<div class="agileits-social">
+					<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#"><i class="fa fa-rss"></i></a></li>
+							<li><a href="#"><i class="fa fa-vk"></i></a></li>
+						</ul>
+				</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+<!--/footer -->
+
+<!-- js -->
+<script type='text/javascript' src='../temp/js/jquery-2.2.3.min.js'></script>
+
+
+
+
+<script src="temp/js/SmoothScroll.min.js"></script>
+<!-- smooth scrolling -->
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	
+	
+	<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //smooth scrolling -->
+	<!-- //for-Clients -->
+	<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="temp/js/move-top.js"></script>
+<script type="text/javascript" src="temp/js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<!-- Calendar -->
+				<script src="temp/js/jquery-ui.js"></script>
+				  <script>
+						  $(function() {
+							$( "#datepicker" ).datepicker();
+						  });
+				  </script>
+			<!-- //Calendar -->
+<script type="text/javascript" src="temp/js/bootstrap-3.1.1.min.js"></script>
+	
+
+</body>
+</html>       
